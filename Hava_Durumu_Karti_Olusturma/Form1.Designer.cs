886988eb4e127fd2c98eba103135d455efde2be5
muhammettozkull <muhammettozkull@gmail.com>
2022-07-08 10:25:36 +0300

    Add project files.

---
+++ Hava_Durumu_Karti_Olusturma/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Hava_Durumu_Karti_Olusturma
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.LblRuzgar = new System.Windows.Forms.Label();
+			this.LblNem = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.LblWeather = new System.Windows.Forms.Label();
+			this.LblYagis = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.LblSicaklik = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(23, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(161, 36);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "IZMIR, TR";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(23, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Tarih";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(23, 127);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 25);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Bugün";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(207, 198);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(242, 91);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "00:00";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(28, 178);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(153, 135);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(390, 189);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(23, 25);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "c";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label6.ForeColor = System.Drawing.Color.Turquoise;
+			this.label6.Location = new System.Drawing.Point(24, 346);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(80, 25);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Rüzgar";
+			// 
+			// LblRuzgar
+			// 
+			this.LblRuzgar.AutoSize = true;
+			this.LblRuzgar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblRuzgar.ForeColor = System.Drawing.Color.White;
+			this.LblRuzgar.Location = new System.Drawing.Point(107, 346);
+			this.LblRuzgar.Name = "LblRuzgar";
+			this.LblRuzgar.Size = new System.Drawing.Size(74, 25);
+			this.LblRuzgar.TabIndex = 8;
+			this.LblRuzgar.Text = "Bugün";
+			// 
+			// LblNem
+			// 
+			this.LblNem.AutoSize = true;
+			this.LblNem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblNem.ForeColor = System.Drawing.Color.White;
+			this.LblNem.Location = new System.Drawing.Point(106, 393);
+			this.LblNem.Name = "LblNem";
+			this.LblNem.Size = new System.Drawing.Size(74, 25);
+			this.LblNem.TabIndex = 10;
+			this.LblNem.Text = "Bugün";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label8.ForeColor = System.Drawing.Color.Turquoise;
+			this.label8.Location = new System.Drawing.Point(23, 393);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(56, 25);
+			this.label8.TabIndex = 9;
+			this.label8.Text = "Nem";
+			// 
+			// LblWeather
+			// 
+			this.LblWeather.AutoSize = true;
+			this.LblWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblWeather.ForeColor = System.Drawing.Color.White;
+			this.LblWeather.Location = new System.Drawing.Point(218, 279);
+			this.LblWeather.Name = "LblWeather";
+			this.LblWeather.Size = new System.Drawing.Size(50, 16);
+			this.LblWeather.TabIndex = 12;
+			this.LblWeather.Text = "Bugün";
+			// 
+			// LblYagis
+			// 
+			this.LblYagis.AutoSize = true;
+			this.LblYagis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblYagis.ForeColor = System.Drawing.Color.White;
+			this.LblYagis.Location = new System.Drawing.Point(107, 441);
+			this.LblYagis.Name = "LblYagis";
+			this.LblYagis.Size = new System.Drawing.Size(49, 25);
+			this.LblYagis.TabIndex = 14;
+			this.LblYagis.Text = "0 %";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label9.ForeColor = System.Drawing.Color.Turquoise;
+			this.label9.Location = new System.Drawing.Point(24, 441);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(66, 25);
+			this.label9.TabIndex = 13;
+			this.label9.Text = "Yağış";
+			// 
+			// LblSicaklik
+			// 
+			this.LblSicaklik.AutoSize = true;
+			this.LblSicaklik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblSicaklik.ForeColor = System.Drawing.Color.White;
+			this.LblSicaklik.Location = new System.Drawing.Point(107, 488);
+			this.LblSicaklik.Name = "LblSicaklik";
+			this.LblSicaklik.Size = new System.Drawing.Size(67, 25);
+			this.LblSicaklik.TabIndex = 16;
+			this.LblSicaklik.Text = "00:00";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label10.ForeColor = System.Drawing.Color.Turquoise;
+			this.label10.Location = new System.Drawing.Point(24, 488);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(87, 25);
+			this.label10.TabIndex = 15;
+			this.label10.Text = "Sıcaklık";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label7.ForeColor = System.Drawing.Color.White;
+			this.label7.Location = new System.Drawing.Point(158, 480);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(15, 16);
+			this.label7.TabIndex = 17;
+			this.label7.Text = "c";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(57)))), ((int)(((byte)(119)))));
+			this.ClientSize = new System.Drawing.Size(467, 564);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.LblSicaklik);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.LblYagis);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.LblWeather);
+			this.Controls.Add(this.LblNem);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.LblRuzgar);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label LblRuzgar;
+		private System.Windows.Forms.Label LblNem;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label LblWeather;
+		private System.Windows.Forms.Label LblYagis;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label LblSicaklik;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label7;
+	}
+}
+
---
+++ Hava_Durumu_Karti_Olusturma/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Hava_Durumu_Karti_Olusturma
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label2.Text = DateTime.Now.ToShortDateString();
+
+			string api = "0c45010d1c81302c946d0622a3898024";
+			string connection = "https://api.openweathermap.org/data/2.5/weather?q=izmir&mode=xml&lang=tr&units=metric&appid=" + api;
+			XDocument hava = XDocument.Load(connection);
+			var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+			var ruzgar = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
+			var nem = hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+			var durum = hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+			label4.Text = sicaklik.ToString();
+			LblSicaklik.Text = sicaklik.ToString();
+			LblRuzgar.Text = ruzgar + " m/s";
+			LblNem.Text = nem + " %";
+			LblWeather.Text = durum.ToUpper();
+			//durum = "bulutlu";
+
+			if (durum == "açık")
+			{
+				pictureBox1.ImageLocation = @"C:\Users\LENOVO\OneDrive\Masaüstü\RESİMLER\Status-weather-clear-icon.png";
+			}
+			if (durum == "bulutlu")
+			{
+				pictureBox1.ImageLocation = @"C:\Users\LENOVO\OneDrive\Masaüstü\RESİMLER\cloudy-icon.png";
+			}
+		}
+	}
+}
